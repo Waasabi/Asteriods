efaf1b1acbe31db42e27c02653c0748961207dfd
waasabi <caldarellianthony@gmail.com>
2024-09-13 21:53:04 -0400

    update ui

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((000, 0, 0)) 
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # Update the display
-    pygame.display.flip()
-
-    # Run the game loop
-    running = True
-    while running:
+  while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-             running = False
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
