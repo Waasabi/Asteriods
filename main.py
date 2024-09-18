2aba01f6c85828a191e10e88937891828e8357af
waasabi <caldarellianthony@gmail.com>
2024-09-18 19:38:41 -0400

    add moving and groups

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
   
@@ -13,16 +13,27 @@ def main():
   clock = pygame.time.Clock()
   dt=0 
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  player1 = player(x,y)
+  
+  
+  updatable= pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable,drawable)
+  Player(x,y)
+  
   while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        player1.draw(screen)
-        pygame.display.flip()
         dt = clock.tick(60)/1000
+
+        for MP in updatable:
+            MP.update(dt)
+        screen.fill("black")
+        
+        for MP in drawable:
+            MP.draw(screen)
+        pygame.display.flip()
+        
         
 if __name__ == "__main__":
     main()
